3f773a230c1e6760c78116869d3894b9b4d7b913
Dayne Broderson <broderson@gmail.com>
2024-03-30 15:31:44 -0800

    initial-commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# d-ruby2d
+
+Experiments in [ruby2d](https://www.ruby2d.com/).
---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# get system deps if on Linux (Ubuntu/Deb)
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+
+# get ruby deps
+bundle
